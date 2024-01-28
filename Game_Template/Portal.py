e4d08c52041bf9ecebcdf526f80aeac35cea5a35
dayujian <154533294+dayujian@users.noreply.github.com>
2024-01-28 20:46:34 +0800

    Update Portal.py

--- Game_Template/Portal.py
+++ Game_Template/Portal.py
@@ -5,14 +5,16 @@ from Settings import *
 import pygame
 
 class Portal(pygame.sprite.Sprite):
-    def __init__(self, x, y, GOTO:SceneType):
+    def __init__(self, GOTO:SceneType,x=PortalSettings.coordX, y=PortalSettings.coordY):
         super().__init__()
         
-        ##### Your Code Here ↓ #####
-        pass
-        ##### Your Code Here ↑ #####
-    
+        self.image=pygame.transform.scale(pygame.image.load(GamePath.portal).convert_alpha(),
+                                          (PortalSettings.width,PortalSettings.height))
+        self.rect=self.image.get_rect(topleft=(x,y))
+        self.destination=GOTO
+        self.mask=pygame.mask.from_surface(self.image)
+
     def draw(self, window, dx=0, dy=0):
-        ##### Your Code Here ↓ #####
-        pass
-        ##### Your Code Here ↑ #####
+        self.rect.x -= dx
+        self.rect.y -= dy
+        window.blit(self.image,self.rect)
